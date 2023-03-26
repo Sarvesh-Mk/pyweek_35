e828b507d2e49dde2984d9a85ec9396a27bad1b1
Icode-python <me.fast.learn@gmail.com>
2023-03-26 12:57:04 -0400

    got boiler plate code done, added gameloop, camera and level loader too

--- main.py
+++ main.py
@@ -1,18 +1,55 @@
-import pygame 
+import pygame, sys
+from settings import *
+from state import *
+from os import path
 
-pygame.init()
-win = pygame.display.set_mode((1620, 880))
+class Game:
+    def __init__(self):
 
-def redrawWindow():
-    win.fill((255, 255, 255))
-    pygame.display.update()
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption(TITLE)
+        self.clock = pygame.time.Clock()
+        self.load_data()
 
-run = True
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit
-            run = False
+    def load_data(self):
+        game_folder = path.dirname(__file__)
+        self.map = Map(path.join(game_folder, "levels/mapexample.txt"))
+    
+    def new(self):
+        self.all_sprites = pygame.sprite.Group()
+        self.camera = Camera(self.map.width, self.map.height)
 
-    redrawWindow()
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.dt = self.clock.tick(FPS) / 1000
+            self.update()
+            self.draw()
+            self.events()
 
+    def update(self):
+        self.all_sprites.update()
+
+    def quit(self):
+        pygame.quit()
+        sys.exit()
+
+    def draw(self):
+        self.screen.fill(BACKGROUND_COLOR)
+        for sprite in self.all_sprites:
+            self.screen.blit(sprite.image, self.camera.apply(sprite))
+        pygame.display.flip()
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    self.quit()
+
+g = Game()
+while True:
+    g.new()
+    g.run()
